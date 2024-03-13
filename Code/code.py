27494c345bfa847b3652b473fbec5731f9ba9fc5
Sayan Bhattacharjee <88587858+Sayan0718@users.noreply.github.com>
2024-03-13 20:41:00 +0530

    Update code.py

--- Code/code.py
+++ Code/code.py
@@ -1,4 +1,18 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+df = pd.read_csv('spam.csv',encoding='ISO-8859-1')
+
+#Data Cleaning
+df.drop(columns=['Unnamed: 2','Unnamed: 3','Unnamed: 4'],inplace=True)
+#renaming the columns
+df.rename(columns={'v1':'target','v2':'text'},inplace=True)
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+df['target'] = encoder.fit_transform(df['target'])
+
+#check missing values
+df.isnull().sum()
+#check for duplicate values
+df.duplicated().sum()
+#remove duplicates
+df = df.drop_duplicates(keep='first')
